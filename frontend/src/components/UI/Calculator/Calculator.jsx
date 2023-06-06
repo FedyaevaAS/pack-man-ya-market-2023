8fb643040b19403b9a0125ee2c01cff2ea80f153
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-06 13:58:40 +0500

    footer renamed to nda

---
+++ frontend/src/components/UI/Calculator/Calculator.jsx
@@ -0,0 +1,11 @@
+import './calculator.scss';
+
+const Calculator = () => {
+  return (
+    <section className="calculator">
+      <div></div>
+    </section>
+  );
+};
+
+export default Calculator;
