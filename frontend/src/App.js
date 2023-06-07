cbbc89a2f34a43a0132ff2bcfe0350e8a1b51856
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-07 18:01:33 +0500

    feat(calculator): styles edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import Nda from './components/Nda/Nda';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { useState } from 'react';
+import Calculator from './components/Calculator/Calculator';
 
 const App = () => {
   return (
     <>
       <Header />
       <h1 style={{ height: '2000px' }}>Hello world</h1>
+      <Calculator />
       <Footer />
       <Nda />
     </>
