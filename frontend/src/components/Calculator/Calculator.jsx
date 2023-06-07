cbc30aa9bea13beb0ee97278ad1e7b56e92df4ec
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-07 17:43:16 +0500

    feat(mainbutton, calculator): create components

--- frontend/src/components/Calculator/Calculator.jsx
+++ frontend/src/components/Calculator/Calculator.jsx
@@ -1,10 +1,57 @@
-import './calculator.scss';
+import React, { useEffect, useState } from 'react';
 
-const Calculator = () => {
+import './calculator.scss';
+import MainButton from '../UI/MainButton/MainButton';
+
+const Calculator = ({ onCalculatorSubmit, isOpen, openPopup }) => {
+  const buttonValue = ['7', '8', '9', '4', '5', '6', '1', '2', '3', 'x', '0'];
+
+  const [disabledValue, setDisabledValue] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  const onChangeInput = (n) => {
+    if (n === 'x') {
+      setInputValue(inputValue.slice(0, -1));
+      return;
+    } else if (inputValue.length === 13) {
+      return;
+    }
+    setInputValue(inputValue + n);
+  };
+
+  const onSubmit = () => {
+    onCalculatorSubmit(inputValue);
+  };
+
+  useEffect(() => {
+    inputValue.length === 13 ? setDisabledValue(false) : setDisabledValue(true);
+  }, [inputValue]);
+
   return (
-    <section className="calculator">
-      <div></div>
-    </section>
+    <div className={`overlay ${isOpen === true ? 'overlay_opened' : null}`}>
+      <section className="calculator">
+        <div className="calculator__flex">
+          <h2>Введите штрихкод товара</h2>
+          <input
+            className={`${disabledValue === true ? 'disabled' : null}`}
+            value={inputValue}
+            readOnly></input>
+          <div>
+            {buttonValue.map((n, i) => {
+              return (
+                <button key={i} onClick={() => onChangeInput(n)}>
+                  {n}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <MainButton onClick={onSubmit} text={'Готово'} disabled={disabledValue} />
+      </section>
+      <div className="calculator__footer">
+        <button onClick={openPopup}>Назад</button>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 import './footer.scss';
 import keyboard from '../../images/keyboard-logo.svg';
 
-const Footer = () => {
+const Footer = ({ openPopup }) => {
   return (
     <footer className="footer">
       <button className="footer__back-button">Назад</button>
-      <button className="footer__keyboard-button">
+      <button onClick={openPopup} className="footer__keyboard-button">
         <img src={keyboard} alt="keyboard-logo" />
         Ввести с клавиатуры
       </button>
