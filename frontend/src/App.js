4c4985acce1f7f2535488c816cd23c67f9a520eb
Anton Aduisky <a.mltw@yandex.ru>
2023-06-09 14:05:29 +0300

    feat: small fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Calculator from './components/Calculator/Calculator';
+// import Calculator from './components/Calculator/Calculator';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Header />
+
         <Routes>
           <Route index element={<MainPage />} />
         </Routes>
         <Footer />
-        <Nda />
       </BrowserRouter>
     </>
   );
