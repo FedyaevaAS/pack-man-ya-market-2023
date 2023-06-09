05bb5d6cc52827b6f325e1dfb1b26f00026fc311
Anton Aduisky <a.mltw@yandex.ru>
2023-06-09 13:46:23 +0300

    feat: order lis is added

---
+++ frontend/src/components/OrderList/OrderList.jsx
@@ -0,0 +1,46 @@
+import styles from './OrderList.module.scss';
+import OrderCard from './components/OrderCard/OrderCard';
+
+const OrderList = ({ order }) => {
+  const orders = [
+    {
+      image: 'https://avatars.mds.yandex.net/get-mpic/1361544/img_id3625151140723044197.jpeg/orig',
+      text: 'Умная колонка Яндекс Станция Лайт, ультрафиолет',
+      tag: 'Пузырчатая плёнка',
+      counter: 3,
+      number: 1231234,
+    },
+    {
+      image: 'https://avatars.mds.yandex.net/get-mpic/1361544/img_id3625151140723044197.jpeg/orig',
+      text: 'Тарелка. Императорский фарфоровый завод. Форма "Стандартная - 2", рисунок "Скарлетт 2". Костяной фарфор . 270 мм.',
+      tag: [['хрупкое'], ['Пузырчатая плёнка']],
+      counter: 2,
+      number: 1231234,
+    },
+    {
+      image: 'https://avatars.mds.yandex.net/get-mpic/1361544/img_id3625151140723044197.jpeg/orig',
+      text: 'Набор для рисования, детский художественный набор в чемоданчике, набор юного художника, 48 предметов и раскраска',
+      tag: ['Пузырчатая плёнка'],
+      counter: 1,
+      number: 1231234,
+    },
+  ];
+
+  return (
+    <>
+      <ul>
+        {orders?.map((order) => (
+          <OrderCard
+            image={order.image}
+            text={order.text}
+            tag={order.tag}
+            counter={`${order.counter} шт.`}
+            number={order.number}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default OrderList;
