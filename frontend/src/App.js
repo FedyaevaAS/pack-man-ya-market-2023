692ba103228269cd0864bb3e367bf6362a193840
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-10 12:58:38 +0500

    feat(efficiency): component is ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { useState } from 'react';
-import Calculator from './components/Calculator/Calculator';
+import Efficiency from './components/Efficiency/Efficiency';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <Header />
-      <h1 style={{ height: '2000px' }}>Hello world</h1>
+      <Header onClick={onClick} />
+      <Efficiency isOpen={isOpen} onClose={onClick} />
+      {!isOpen && <h1 style={{ height: '2000px', display: 'block' }}>Hello world</h1>}
       <Footer />
     </>
   );
