6eec6d698d57f97348a7e19acb5e48293ecbaa2c
Anton Aduisky <a.mltw@yandex.ru>
2023-06-10 22:15:08 +0300

    feat: current date is added

--- frontend/src/components/Nda/Nda.jsx
+++ frontend/src/components/Nda/Nda.jsx
@@ -1,13 +1,16 @@
 import './nda.scss';
 
 const Nda = ({ forCalculator }) => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <section className={`nda ${forCalculator ? 'nda_static' : null}`}>
       <div>
         <p>Коммерческая тайна ООО «Яндекс», 119021, Россия, г. Москва, ул. Льва Толстого, д. 16</p>
         <p>2022.11.1 / 2023.1.68</p>
       </div>
-      <p>© 2003–2023 ООО «Яндекс»</p>
+      <p>© 2003–{year} ООО «Яндекс»</p>
     </section>
   );
 };
