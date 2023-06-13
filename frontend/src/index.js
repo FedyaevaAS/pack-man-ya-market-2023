3be10a1ca364804a9f11ded8bf7ecd8f3ba15229
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-13 13:38:26 +0500

    project ready to test deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+// need to change BrowserRouter to HashRouter for gh-pages
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
