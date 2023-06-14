a7856e4bbbe5f861b8eaa5c23a2884aae4c9cb5b
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-14 16:05:15 +0500

    feat(mainPage): add logic with scan package

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './MainPage.module.scss';
 import jsonData from '../../vendor/styles.json';
 import PackageButton from '../../components/UI/PackageButton/PackageButton';
@@ -13,12 +13,17 @@ import { Link } from 'react-router-dom';
 const MainPage = ({ efficiencyIsOpen }) => {
   const issueButtonNames = ['Сломан монитор', 'Сломан сканер', 'Сломан принтер'];
   const cancelButtonNames = ['Нет товара', 'Несоответствие товара', 'Дефект упаковки'];
+
+  const totalPackageCount = useRef(0);
+  const scannedPackages = useRef(0);
+
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [isCalculatorOpen, setIsCalculatorOpen] = useState(false);
   const [isIssueButtonsOpen, setIsIssueButtonsOpen] = useState(false);
   const [isCancelButtonsOpen, setIsCancelButtonsOpen] = useState(false);
   const [isRecommendationsOpen, setIsRecommendationsOpen] = useState(false);
   const [isOrderScanned, setIsOrderScanned] = useState(false);
+  const [isPackageScanned, setIsPackageScanned] = useState(false);
 
   isPopupOpen || efficiencyIsOpen
     ? (document.body.style.overflowY = 'hidden')
@@ -45,9 +50,21 @@ const MainPage = ({ efficiencyIsOpen }) => {
     setIsPopupOpen(false);
   };
 
-  const setAllScanned = () => {
+  const setAllItemsScanned = () => {
     setIsOrderScanned(true);
   };
+
+  const setAllPackageScanned = (scanCount) => {
+    scannedPackages.current = scannedPackages.current + scanCount;
+
+    if (scannedPackages.current === totalPackageCount.current) {
+      setIsPackageScanned(true);
+    }
+  };
+
+  useEffect(() => {
+    totalPackageCount.current = Object.keys(jsonData).length;
+  }, []);
 
   return (
     <>
@@ -60,20 +77,25 @@ const MainPage = ({ efficiencyIsOpen }) => {
           <div className={styles.heading}>
             <h1 className={styles.heading__title}>Сканируйте товары</h1>
             <h2 className={styles.heading__order}>B - 63626</h2>
-            <ul className={styles.heading__badges} onClick={() => handleOpenPopups('recommend')}>
+            <ul className={styles.heading__badges}>
               {Object.keys(jsonData).map((key) => (
-                <PackageButton key={key} boxType={key} packageData={jsonData[key]} />
+                <PackageButton
+                  key={key}
+                  boxType={key}
+                  packageData={jsonData[key]}
+                  setAllScanned={setAllPackageScanned}
+                />
               ))}
             </ul>
           </div>
           <OrderList
             onCancelClick={() => handleOpenPopups('cancel')}
-            isAllScanned={setAllScanned}
+            isAllScanned={setAllItemsScanned}
           />
         </div>
         {isOrderScanned && (
-          <Link to={'/success'}>
-            <MainButton text={'Готово'} />
+          <Link to={isOrderScanned && isPackageScanned ? '/success' : '/main'}>
+            <MainButton text={'Готово'} onClick={() => handleOpenPopups('recommend')} />
           </Link>
         )}
       </div>
