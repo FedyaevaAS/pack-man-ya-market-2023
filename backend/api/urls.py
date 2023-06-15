ef08dd0e37820bc02c586014759f8ba9fa52ce78
umar1593 <umar1593@mail.ru>
2023-06-15 11:41:18 +0300

    создал неколько ручек

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,26 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
-from .views import OrderViewSet
-
-router = DefaultRouter()
-router.register('orders', OrderViewSet)
+from .views import CancelOrder, GenerateOrderID, GetByOrderId, MarkOrderAsOK
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path(
+        'generate_order_id/',
+        GenerateOrderID.as_view(),
+        name='generate_order_id',
+    ),
+    path(
+        'cancel_order/<uuid:order_key>/',
+        CancelOrder.as_view(),
+        name='cancel_order',
+    ),
+    path(
+        'mark_order_as_ok/<uuid:order_key>/',
+        MarkOrderAsOK.as_view(),
+        name='mark_order_as_ok',
+    ),
+    path(
+        'get_by_order_id/<uuid:order_key>/',
+        GetByOrderId.as_view(),
+        name='get_by_order_id',
+    ),
 ]
