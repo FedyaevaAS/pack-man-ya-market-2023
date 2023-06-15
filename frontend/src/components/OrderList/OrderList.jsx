fbefe75170a510e9c78987066d3f5c9a335659cc
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-15 13:32:55 +0500

    feat(mainPage): create keyboard logic

--- frontend/src/components/OrderList/OrderList.jsx
+++ frontend/src/components/OrderList/OrderList.jsx
@@ -8,30 +8,31 @@ const orders = [
     text: 'Умная колонка Яндекс Станция Лайт, ультрафиолет',
     tag: 'Пузырчатая плёнка',
     counter: 3,
-    number: 1231234
+    number: '1234 5678 234 32'
   },
   {
     image: 'https://avatars.mds.yandex.net/get-mpic/1361544/img_id3625151140723044197.jpeg/orig',
     text: 'Тарелка. Императорский фарфоровый завод. Форма "Стандартная - 2", рисунок "Скарлетт 2". Костяной фарфор . 270 мм.',
     tag: [['хрупкое'], ['Пузырчатая плёнка']],
     counter: 2,
-    number: 1231234
+    number: '1234 5678 234 33'
   },
   {
     image: 'https://avatars.mds.yandex.net/get-mpic/1361544/img_id3625151140723044197.jpeg/orig',
     text: 'Набор для рисования, детский художественный набор в чемоданчике, набор юного художника, 48 предметов и раскраска',
     tag: ['Пузырчатая плёнка'],
     counter: 1,
-    number: 1231234
+    number: '1234 5678 234 34'
   }
 ];
 
-const OrderList = ({ order, onCancelClick, isAllScanned }) => {
+const OrderList = ({ order, onCancelClick, isAllScanned, calculatorValue }) => {
   const totalCount = useRef(0);
   const scanned = useRef(0);
 
   const onScanSubmit = (scanCount) => {
     scanned.current = scanned.current + scanCount;
+    console.log(scanned.current);
 
     if (scanned.current === totalCount.current) {
       isAllScanned();
@@ -58,6 +59,7 @@ const OrderList = ({ order, onCancelClick, isAllScanned }) => {
             onCancelClick={onCancelClick}
             isExpanded={false}
             onScanSubmit={onScanSubmit}
+            calculatorValue={calculatorValue}
           />
         ))}
       </ul>
