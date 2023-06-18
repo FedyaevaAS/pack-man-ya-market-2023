d3762add678a54e0e5440b43f2b836ca82800cb3
Anton Aduisky <a.mltw@yandex.ru>
2023-06-18 22:05:49 +0300

    feat:  isCanceled logic is added

--- frontend/src/components/UI/CounterButton/CounterButton.jsx
+++ frontend/src/components/UI/CounterButton/CounterButton.jsx
@@ -8,7 +8,8 @@ const CounterButton = ({
   scanned,
   onScanSubmit,
   number,
-  calculatorValue
+  calculatorValue,
+  isCanceled,
 }) => {
   const [isScanned, setIsScanned] = useState(false);
 
@@ -30,7 +31,9 @@ const CounterButton = ({
 
   return (
     <button
-      className={`${styles.counter} ${isScanned || scanned ? styles.scanned : ''}`}
+      className={`${isCanceled ? styles.disabled : styles.counter} ${
+        isScanned || scanned ? styles.scanned : ''
+      }`}
       onClick={handleClick}
       disabled={disabled || isScanned}>
       {counter} шт.
