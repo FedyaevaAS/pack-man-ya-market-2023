8af1b34b27fbb65cd31a9d355f75822337c8e26e
Alexandr Kolganov <gengizar@yandex.ru>
2023-06-19 06:11:03 +0500

    feat(recommendations): add logic with packages

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -20,6 +20,7 @@ const MainPage = ({ efficiencyIsOpen }) => {
 
   const totalPackageCount = useRef(0);
   const scannedPackages = useRef(0);
+  const packageRecommendationCount = useRef(0);
 
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [isCalculatorOpen, setIsCalculatorOpen] = useState(false);
@@ -75,8 +76,11 @@ const MainPage = ({ efficiencyIsOpen }) => {
     handleClosePopups();
   };
 
-  const changePackageRecommendation = () => {
-    console.log('changePackageRecommendation');
+  const changePackageRecommendation = (plus) => {
+    plus === 'plus'
+      ? (packageRecommendationCount.current = packageRecommendationCount.current + 1)
+      : (packageRecommendationCount.current = packageRecommendationCount.current - 1);
+    handleClosePopups();
   };
 
   useEffect(() => {
@@ -119,7 +123,7 @@ const MainPage = ({ efficiencyIsOpen }) => {
                     }`}
                   />
                   <MainHeadingBadge text={order.delivery_type} />
-                  {Object.keys(Object.assign({}, ...order.packages)).map((key) => (
+                  {Object.keys(order.packages[packageRecommendationCount.current]).map((key) => (
                     <PackageButton key={key} boxType={key} setAllScanned={setAllPackageScanned} />
                   ))}
                 </ul>
@@ -150,6 +154,7 @@ const MainPage = ({ efficiencyIsOpen }) => {
         isOpen={isRecommendationsOpen}
         onBackClick={handleClosePopups}
         changePackage={changePackageRecommendation}
+        packageRecommendationCount={packageRecommendationCount.current}
       />
       {!isRecommendationsOpen && (
         <ControlPanel
